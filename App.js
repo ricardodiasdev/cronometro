896aefd31ab627d65ef509b05374b29d696a73f0
Ricardo Dias <ricardodias.dev@gmail.com>
2023-03-07 10:29:37 -0300

    Lógica e finalização do app

--- App.js
+++ App.js
@@ -1,18 +1,52 @@
+import { useState } from "react";
 import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 import cronometro from "./src/assets/cronometro.png";
 export default function App() {
+  const [numero, setNumero] = useState(0);
+  const [ultimo, setUltimo] = useState(0);
+  const [timer, setTimer] = useState(null);
+  const [isStop, setIsStop] = useState(true);
+
+  const handleStartorStop = () => {
+    if (timer !== null) {
+      clearInterval(timer);
+      setTimer(null);
+      setIsStop(true);
+    } else {
+      setTimer(
+        setInterval(() => {
+          setNumero((numero) => numero + 0.1);
+        }, 100)
+      );
+      setIsStop(false);
+    }
+  };
+
+  const handleReset = () => {
+    if (timer !== null) {
+      clearInterval(timer);
+      setTimer(null);
+    }
+    setIsStop(true);
+    setUltimo(numero);
+    setNumero(0);
+  };
+
   return (
     <View style={styles.container}>
       <Image style={styles.image} source={cronometro} />
-      <Text style={styles.numbers}>0.0</Text>
+      <Text style={styles.numbers}>{numero.toFixed(1)}</Text>
       <View style={styles.btnArea}>
-        <TouchableOpacity style={styles.btn}>
-          <Text style={styles.btnText}>START</Text>
+        <TouchableOpacity style={styles.btn} onPress={handleStartorStop}>
+          <Text style={styles.btnText}>{isStop ? "Start" : "Stop"}</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.btn}>
+        <TouchableOpacity style={styles.btn} onPress={handleReset}>
           <Text style={styles.btnText}>CLEAR</Text>
         </TouchableOpacity>
       </View>
+      <Text style={styles.text}>
+        {ultimo > 0 ? `Último intervalo de tempo: ${ultimo.toFixed(1)} segundos` : ""}
+      </Text>
     </View>
   );
 }
@@ -24,32 +58,36 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  image:{
-
-  },
-  numbers:{
+  image: {},
+  numbers: {
     marginTop: -160,
-    color:'#FFF',
+    color: "#FFF",
     fontSize: 65,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
-  btnArea:{
+  btnArea: {
     flexDirection: "row",
-    marginTop: 70, 
+    marginTop: 70,
     height: 40,
   },
-  btn:{
+  btn: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
     height: 40,
     margin: 17,
     borderRadius: 9,
   },
-  btnText:{
+  btnText: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     color: "#00aeef",
   },
+  text:{
+    marginTop: 40,
+    color: "#FFF",
+    fontSize: 15,
+    fontWeight: "bold",
+  }
 });
